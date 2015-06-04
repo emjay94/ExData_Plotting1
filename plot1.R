6f126c0cfad000ae7807bd27b4e1c3409213cfb8
emjay94 <moojsuh@gmail.com>
2015-06-04 18:27:57 +0900

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,12 @@
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile="./data/household_power_consumption.zip")
+dataset <- unzip("./data/household_power_consumption.zip", list=TRUE)$Name
+unzip("./data/household_power_consumption.zip")
+powerconsumption <- read.table(dataset, header=TRUE, sep=";", na.strings="?")
+renamed <- gsub("_", ".", names(powerconsumption))
+renamed <- tolower(renamed)
+names(powerconsumption) <- renamed
+powerconsumption$date <- as.Date(powerconsumption$date, "%d/%m/%Y")
+powerconsump2days <- subset(powerconsumption, date=="2007-02-01" | date=="2007-02-02")
+powerconsump2days$date.time <- strptime(c(powerconsump2days$date, powerconsump2days$time), "%Y-%m-%d %H:%M:%S")
+save(powerconsump2days, file="powerconsump2days.RData")
